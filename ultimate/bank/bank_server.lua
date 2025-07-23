d486292f73718ea74095fc5c44b360178188e34e
Hungryy2K <86595306+Hungryy2K@users.noreply.github.com>
2025-07-23 17:23:44 +0200

    Create bank_server.lua

---
+++ ultimate/bank/bank_server.lua
@@ -0,0 +1,69 @@
+local Logger = require("utility.Logger")
+local adminLogger = Logger:new("Admin")
+local securityLogger = Logger:new("Security")
+local discordLogger = Logger:new("Discord")
+
+-- Tabelle für gebannte Bank-Accounts
+bankBlacklist = {}
+
+function isAbuseTransfer(sender, receiver)
+    local senderIP = getPlayerIP(sender)
+    local receiverIP = getPlayerIP(receiver)
+    local senderSerial = getPlayerSerial(sender)
+    local receiverSerial = getPlayerSerial(receiver)
+    if senderIP == receiverIP or senderSerial == receiverSerial then
+        return true
+    end
+    return false
+end
+
+function canUseBank(player)
+    return not bankBlacklist[getPlayerName(player)]
+end
+
+function adminBanBankAccount(admin, player)
+    local pname = getPlayerName(player)
+    bankBlacklist[pname] = true
+    adminLogger:info("[BANK-ADMIN] "..getPlayerName(admin).." hat das Bankkonto von "..pname.." gesperrt.")
+    outputChatBox("Das Bankkonto von "..pname.." wurde gesperrt.", admin, 255, 0, 0)
+end
+
+function adminUnbanBankAccount(admin, pname)
+    bankBlacklist[pname] = nil
+    adminLogger:info("[BANK-ADMIN] "..getPlayerName(admin).." hat das Bankkonto von "..pname.." entsperrt.")
+    outputChatBox("Das Bankkonto von "..pname.." wurde entsperrt.", admin, 0, 255, 0)
+end
+
+addCommandHandler("bankban", function(admin, cmd, targetName)
+    local target = findPlayerByName(targetName)
+    if target then
+        adminBanBankAccount(admin, target)
+    else
+        outputChatBox("Spieler nicht gefunden!", admin, 255, 0, 0)
+    end
+end)
+
+addCommandHandler("bankunban", function(admin, cmd, targetName)
+    adminUnbanBankAccount(admin, targetName)
+end)
+
+-- Beispiel für Banküberweisung mit Abuse-Check und Blacklist
+function transferMoney(sender, receiver, amount)
+    if not canUseBank(sender) then
+        outputChatBox("Dein Bankkonto ist gesperrt. Bitte Support kontaktieren.", sender, 255, 0, 0)
+        return false
+    end
+    if not canUseBank(receiver) then
+        outputChatBox("Das Zielkonto ist gesperrt. Bitte Support kontaktieren.", sender, 255, 0, 0)
+        return false
+    end
+    if isAbuseTransfer(sender, receiver) then
+        securityLogger:error("[BANK-ABUSE] Verdächtige Überweisung: "..getPlayerName(sender).." -> "..getPlayerName(receiver).." ("..amount..")")
+        outputChatBox("Überweisung aus Sicherheitsgründen abgelehnt. Support kontaktieren.", sender, 255, 0, 0)
+        discordLogger:discord("BANK-ABUSE: "..getPlayerName(sender).." -> "..getPlayerName(receiver).." ("..amount..")")
+        return false
+    end
+    -- ... Restliche Prüfungen und eigentliche Überweisung ...
+    -- (Hier folgt die eigentliche Logik für den Geldtransfer)
+    return true
+end 
